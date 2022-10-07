af0108d301ea048517475f545d93590171cf8588
Saad <saad.ali@dastgyr.com>
2022-10-07 15:20:26 +0500

    Made changes in Driver

--- src/modules/batch-delivery/domain ❤️/entities/order-batch.entity.ts
+++ src/modules/batch-delivery/domain ❤️/entities/order-batch.entity.ts
@@ -1,7 +1,12 @@
 import { Entity } from "src/lib/entities/entity";
 
 export class OrderBatch extends Entity {
-
+    public id: number;
+    private date: Date;
+    
+    constructor(id: number) {
+        super(id)
+    }
 
     public validate(): void {
         throw new Error("Method not implemented.");
